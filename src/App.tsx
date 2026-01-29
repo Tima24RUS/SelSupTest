0eef8e46ca227f13152b694ce77634da09dcb519
Tima24RUS <workshorokhovt@gmail.com>
2026-01-29 14:05:12 +0700

    add interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Param {
+  id: number;
+  name: string;
+  type: 'string';
 }
 
-export default App
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Color {
+  id: number;
+  name: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Редактор параметров</h1>
+    </div>
+  )
+};
+
+export default App;
+export type {Param, ParamValue, Color, Model, Props};
